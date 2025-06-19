b503a9c497e89109a16922fb1e42761d4b022dde
PiaCOS <elco@odoo.com>
2025-06-19 13:39:33 +0200

    imp: first gyros commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::process::{ Command, Stdio, Output };
 use std::io::{ self, Write };
 use clap::Parser;
+use colored::Colorize;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -22,87 +23,97 @@ struct Args {
     grep: Option<String>,
 }
 
-fn gyros() -> () {
-    println!(">-------------------------------------------------------------------------------<");
-    println!(">---<>---<>---<>---<>---<>---<>---< G Y R O S >---<>---<>---<>---<>---<>---<>---<");
-    println!(">-------------------------------------------------------------------------------<");
+fn gyros() {
+    println!("{}", ">-------------------------------------------------------------------------------<".yellow());
+    println!("{}", ">---<>---<>---<>---<>---<>---<>---< G Y R O S >---<>---<>---<>---<>---<>---<>---<".yellow());
+    println!("{}", ">-------------------------------------------------------------------------------<".yellow());
 }
 
-// TODO:
-// -> use clap to have git_diff on -d
-// -> create another command with -p
-// -> create another command with -c
-//      => ask for a branch name
-//      => if it doesnt exists in one repo: ask which branch for the other
-//      => if both doesn't exists: ask again
-//      => if changes
-//          ==> raise error
-//      => checkout
+// ------------ RUNNERS ------------
 
-fn git_diff(dir: &str) -> io::Result<()> {
-    println!("\n<>--------<> DIFF -> {} <>--------<>", &dir);
-    execute(
-        Command::new("git")
-            .args(&["diff", "--color=always"])
-            .current_dir(dir)
-            .output()
-            .expect("Sorry.. Can't do that :5\n")
-    )
-}
-
-fn git_pull(dir: &str) -> io::Result<()> {
-    println!("\n<>--------<> PULL -> {} <>--------<>", &dir);
-    execute(
-        Command::new("git")
-            .args(&["pull"])
-            .current_dir(dir)
-            .output() 
-            .expect("Sorry.. Can't do that :5\n")
-    )
-}
-
-fn git_checkout(dir: &str, branch: &str) -> io::Result<()> {
-    println!("\n<>--------<> CHECKOUT {} -> {} <>--------<>", &dir, &branch);
-    execute(
-        Command::new("git")
-            .args(&["checkout", &branch])
-            .current_dir(&dir)
-            .output()
-            .expect(&format!("Sorry.. {} do no exist on {}", &branch, &dir))
-        )
-}
-
-
-fn git_grep_branch(dir: &str, grep: &str) -> io::Result<()> {
-    println!("\n<>--------<> GREP BRANCH {} -> {} <>--------<>", &dir, &grep);
-    
-    let gbranch = Command::new("git")
-            // .args(&["branch", "-r", "--color=always"])
-            .args(&["branch", "--color=always"])
-            .current_dir(&dir)
-            .stdout(Stdio::piped())
-            .spawn()
-            .unwrap();
-    execute(
-        Command::new("grep")
-            .arg(&grep)
-            .stdin(Stdio::from(gbranch.stdout.unwrap()))
-            .output()
-            .expect(&format!("Sorry.. Cant do that :5"))
-        )
+fn safe_run(cmd: &mut Command) -> io::Result<Output> {
+    match cmd.output() {
+        Ok(output) => Ok(output),
+        Err(e) => {
+            println!("{}", "Sorry.. Can't do that ;-; ~".red().italic());
+            Err(e)
+        }
+    }
 }
 
 fn execute(cmd: Output) -> io::Result<()> {
-    println!("{}\n", cmd.status);
     io::stdout().write_all(&cmd.stdout)?;
     io::stderr().write_all(&cmd.stderr)?;
     Ok(())
 }
 
+fn run_git_command(dir :&str, args: &[&str]) -> io::Result<()> {
+    let mut cmd = Command::new("git");
+    cmd.args(args).current_dir(dir);
+    let output = safe_run(&mut cmd)?;
+    execute(output)
+}
+
+// ------------ COMMANDS ------------
+
+fn git_diff(dir: &str) -> io::Result<()> {
+    println!("{}", format!("\n<>--------<> DIFF -> {} <>--------<>", dir).green().bold());
+    run_git_command(dir, &["diff", "--color=always", "--name-status"])
+}
+
+fn git_pull(dir: &str) -> io::Result<()> {
+    println!("{}", format!("\n<>--------<> PULL -> {} <>--------<>", dir).green().bold());
+    run_git_command(dir, &["pull"])
+}
+
+fn git_checkout(dir: &str, branch: &str) -> io::Result<()> {
+    println!("{}", format!("\n<>--------<> CHECKOUT {} -> {} <>--------<>", dir, branch).green().bold());
+    
+    let mut cmd = Command::new("git");
+    cmd.args(["checkout", branch]).current_dir(dir);
+    let output = safe_run(&mut cmd)?;
+
+    if output.status.success() {
+        // println!("{}", format!("> Checked out branch '{}' ~\n", branch).purple());
+        execute(output)
+    } else {
+        println!("{}", format!("Sorry.. '{}' does not exist on '{}'..", branch, dir).red().italic());
+        println!("{}", "> Failling back to 'master' ~".purple());
+
+        let mut fallback_cmd = Command::new("git");
+        fallback_cmd.args(["checkout", "master"]).current_dir(dir);
+        let fallback = safe_run(&mut fallback_cmd)?;
+
+        if fallback.status.success() {
+            // println!("{}", "> Checked out fallback 'master' branch ~\n".purple());
+            execute(fallback)
+        } else {
+            println!("{}", "Sorry.. master branch did not work, you may need to check what is going on here ~".red().italic());
+            Err(io::Error::new(io::ErrorKind::Other, "Failed to checkout branch and fallback"))
+        }
+    }
+}
+
+fn git_grep_branch(dir: &str, grep: &str) -> io::Result<()> {
+    println!("{}", format!("\n<>--------<> GREP BRANCH {} -> {} <>--------<>", dir, grep).green().bold());
+
+    let gbranch = Command::new("git")
+        .args(["branch", "--color=always"])
+        .current_dir(dir)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Sorry.. Can't spawn git branch");
+
+    let mut grep_cmd = Command::new("grep");
+    grep_cmd.arg(grep).stdin(Stdio::from(gbranch.stdout.unwrap()));
+    let output = safe_run(&mut grep_cmd)?;
+    execute(output)
+}
+
 fn main() -> io::Result<()> {
     let dir1 = "/home/odoo/Dev/odoo/community/";
     let dir2 = "/home/odoo/Dev/odoo/enterprise/";
-   
+
     let args = Args::parse();
 
     match args {
@@ -126,9 +137,8 @@ fn main() -> io::Result<()> {
             git_grep_branch(dir1, &gr)?;
             git_grep_branch(dir2, &gr)?;
         }
-        _ => println!("<>---<> Exactly one instruction should be given to me :3 <>---<>")
+        _ => println!("{}", "<>---<> Exactly one instruction should be given to me :3 <>---<>".red().italic())
     }
 
     Ok(())
 }
-
