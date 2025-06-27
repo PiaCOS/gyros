706d3eeec0852fe39c724c01d43398ad4de2a4ee
PiaCOS <elco@odoo.com>
2025-06-27 10:10:14 +0200

    imp: handling for single repo operations

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::env;
 use std::fs;
 use std::io::{self, Write};
 use std::process::{Command, Output};
-
+use clap::Parser;
+use std::path::PathBuf;
 
 // TODO:
 //      - add status, log?, stash, fetch, add/commit?
@@ -16,9 +17,17 @@ use std::process::{Command, Output};
 //      - interactive prompt ?
 //      - customizable args
 
-fn get_args() -> Vec<String> {
-    let args: Vec<String> = env::args().collect();
-    args
+#[derive(Parser, Debug)]
+#[command(name = "gyros")]
+#[command(about = "Run git commands in multiple repos", long_about = None)]
+struct Args {
+    /// Only run command in this repo (partial match)
+    #[arg(long)]
+    only: Option<String>,
+
+    /// The git subcommand and args, e.g. status, log, etc.
+    #[arg(required = true, trailing_var_arg = true)]
+    git_args: Vec<String>,
 }
 
 // ------------ RUNNERS ------------
@@ -27,7 +36,7 @@ fn safe_run(cmd: &mut Command) -> io::Result<Output> {
     match cmd.output() {
         Ok(output) => Ok(output),
         Err(e) => {
-            println!("{}", "Sorry.. Can't do that ;-; ~".red().italic());
+            eprintln!("{}", "Sorry.. Can't do that ;-; ~".red().italic());
             Err(e)
         }
     }
@@ -58,8 +67,8 @@ fn print_header(label: &str, repo: &str) {
 // ------------ COMMANDS ------------
 
 fn command(dir: &str, args: &Vec<String>) -> io::Result<()> {
-    print_header(&args[1], dir);
-    run_git_command(dir, &args[1..])
+    print_header(&args[0], dir);
+    run_git_command(dir, &args)
 } 
 
 // ------------- TOML -------------
@@ -88,14 +97,41 @@ fn load() -> io::Result<Vec<String>> {
     Ok(data.config.repos_list)
 }
 
+// ------------- REPO -------------
+
+fn assert_is_repo(path: &str, repo: &str) -> bool {
+    let folder = PathBuf::from(path);
+    if let Some(folder_name) = folder.file_name() {
+        folder_name == repo
+    } else {
+        false
+    }
+}
+
 // ------------- RUN --------------
 
 fn main() -> io::Result<()> {
     let repos_path = load()?;
-    let args = get_args();
+    let args = Args::parse();
 
-    for r in repos_path {
-        command(&r, &args)?;
+    let selected_repos: Vec<String> = match args.only {
+        Some(repo) => {
+            let filtered: Vec<String> = repos_path
+                .into_iter()
+                .filter(|r| assert_is_repo(&r, &repo))
+                .collect();
+            if filtered.len() > 0 {
+                filtered
+            } else {
+                eprintln!("{}", format!("Coundn't find the repo named '{}' ;-;", repo).red().italic());
+                std::process::exit(1);
+            }
+        },
+        None => repos_path,
+    };
+    println!("{:?} -- {:?}", selected_repos, args.git_args);
+    for r in selected_repos {
+        command(&r, &args.git_args)?;
     }
     Ok(())
 }
